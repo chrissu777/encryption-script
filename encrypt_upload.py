bd94aa165007500c07ef2d9c03e065d787050280
chrissu777 <christophersu777@gmail.com>
2025-02-18 13:15:58 -0500

    rewrote encryption logic

--- encrypt_upload.py
+++ encrypt_upload.py
@@ -1,27 +1,41 @@
 import boto3
 import aws_encryption_sdk
+import aws_encryption_sdk.key_providers.kms
 from aws_encryption_sdk.identifiers import CommitmentPolicy
 from aws_encryption_sdk.streaming_client import StreamEncryptor
 from aws_cryptographic_material_providers.mpl.models import CreateAwsKmsKeyringInput
-from config import S3_BUCKET, KMS_KEY_ARN
-
+from aws_cryptographic_material_providers.mpl import AwsCryptographicMaterialProviders
+from aws_cryptographic_material_providers.mpl.config import MaterialProvidersConfig
+from aws_cryptographic_material_providers.mpl.models import CreateAwsKmsKeyringInput
+from aws_cryptographic_material_providers.mpl.references import IKeyring
+S3_BUCKET = 'weaponwatch-demo'
+KMS_KEY_ARN = 'arn:aws:kms:us-east-1:203918845922:key/f0e18996-d4a0-49f6-827d-cc8915c5f864'
 s3_client = boto3.client('s3')
-kms_key_provider = KMSMasterKeyProvider(key_ids=[KMS_KEY_ARN])
 
 def encrypt_and_upload(file_path, s3_key):
+    client = aws_encryption_sdk.EncryptionSDKClient(
+        commitment_policy=CommitmentPolicy.REQUIRE_ENCRYPT_REQUIRE_DECRYPT
+    )
+    kms_client = boto3.client('kms', region_name="us-east-1")
+    mat_prov: AwsCryptographicMaterialProviders = AwsCryptographicMaterialProviders(
+        config=MaterialProvidersConfig()
+    )
+
+    keyring_input: CreateAwsKmsKeyringInput = CreateAwsKmsKeyringInput(
+        kms_key_id=KMS_KEY_ARN,
+        kms_client=kms_client
+    )
+    kms_keyring: IKeyring = mat_prov.create_aws_kms_keyring(
+        input=keyring_input
+    )
     try:
         with open(file_path, "rb") as infile:
-            encryptor = StreamEncryptor(
-                source=infile,
-                key_provider=kms_key_provider,
-                commitment_policy=CommitmentPolicy.REQUIRE_ENCRYPT_REQUIRE_DECRYPT
+            ciphertext, _ = client.encrypt(
+            source=infile,
+            keyring=kms_keyring,
             )
 
-            encrypted_chunks = bytearray()
-            for chunk in encryptor:
-                encrypted_chunks.extend(chunk)
-
-        s3_client.put_object(Bucket=S3_BUCKET, Key=s3_key, Body=bytes(encrypted_chunks))
+        s3_client.put_object(Bucket=S3_BUCKET, Key=s3_key, Body=bytes(ciphertext))
         print(f"File uploaded and encrypted successfully: {s3_key}")
 
     except Exception as e:
